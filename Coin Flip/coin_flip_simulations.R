5c1248d9f85c019994c377621badfc9ccc93a37b
Katleho Mofokeng <katlehom814@gmail.com>
2025-04-10 15:15:19 +0200

    Basic Simulating

--- Coin Flip/coin_flip_simulations.R
+++ Coin Flip/coin_flip_simulations.R
@@ -2,3 +2,15 @@
 # COIN FLIP SIMULATION
 # LAW of Large Numbers Demo
 # ===================
+
+library(ggplot2)
+
+#This is for reproducibilty
+set.seed(123)
+
+n_flips <- 1000 # Total number of coin flips
+
+# 0 -> tails and 1 -> heads
+coin_outcome <- c(0,1)
+
+flips <- sample(coin_outcome, size=n_flips, replace=TRUE)
